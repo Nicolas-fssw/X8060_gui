ccdbc4f1168adad72552479709d41513b580fa46
unknown <nicolas@froresystems.com>
2021-01-25 13:03:34 -0800

    added graphs and graphing capability

--- X8060gui.py
+++ X8060gui.py
@@ -37,7 +37,21 @@ class X8060GUI(QMainWindow):
         self.state_2.setStyleSheet("background-color:lightblue;  border: 1px solid black;")  #save indicator
         self.state_2.setAlignment(QtCore.Qt.AlignCenter)
         
+        self.state_3.setText('Nothing Measured')  
+        self.state_3.setStyleSheet("background-color:lightblue;  border: 1px solid black;")  #save indicator
+        self.state_3.setAlignment(QtCore.Qt.AlignCenter)
+        
+        self.plot_1.setBackground('w')
+        self.plot_2.setBackground('w')
+        self.plot_3.setBackground('w')
+        self.plot_4.setBackground('w')
+        self.plot_5.setBackground('w')
+        self.plot_6.setBackground('w')
+        self.plot_7.setBackground('w')
+        self.plot_8.setBackground('w')
+        
         self.data = []
+        self.summary = []
         self.inputList = []
         self.names = []
         
@@ -53,6 +67,15 @@ class X8060GUI(QMainWindow):
         self.export_3.clicked.connect(self.export_3_click)
         
     def measure_1_click(self):
+        
+        self.plot_1.clear()
+        self.plot_2.clear()
+        self.plot_3.clear()
+        self.plot_4.clear()
+        self.plot_5.clear()
+        self.plot_6.clear()
+        self.plot_7.clear()
+        self.plot_8.clear()
         
         if self.eightbyeight_1.isChecked(): 
             if self.frame_1.isChecked():
@@ -84,18 +107,44 @@ class X8060GUI(QMainWindow):
         self.names = ['1LBCH','1LF2A','1RBCH','1RF2A','2LBCH','2LF2A','2RBCH','2RF2A','3LBCH','3LF2A','3RBCH','3RF2A','4LBCH','4LF2A','4RBCH','4RF2A']
         self.data = readTextFile(path,expected,self.names)
         
-        graphData = []
-        for n in range(0,len(self.data),2):
-            graphData.append([np.average(self.data[n]), max(self.data[n]) - min(self.data[n]), np.average(self.data[n+1]), max(self.data[n+1]) - min(self.data[n+1])])
+        self.summary = [[],[],[],[],[],[],[],[]]
+        count = 0
+        for i in range(0,len(self.data),2):
+            self.summary[count].append([np.average(self.data[i]), np.average(self.data[i+1])])
+            count = count+1
             
-        for n in range(len(graphData)):
-            for i in range(len(graphData[n])):
-                self.tableWidget_1.setItem(i+1, n+1, QTableWidgetItem('n/a'))
-         
-        for n in range(len(graphData)):
-            for i in range(len(graphData[n])):
-                self.tableWidget_1.setItem(i+1, n+1, QTableWidgetItem('%.3f' % graphData[n][i]))
-                
+            
+        self.summary.append([np.average([item[0] for item in self.summary], np.average([item[1] for item in self.summary]))])
+        
+        self.plot_1.plot(range(len(self.data[0])), self.data[0])
+        self.plot_2.plot(range(len(self.data[2])), self.data[2])
+        self.plot_3.plot(range(len(self.data[4])), self.data[4])
+        self.plot_4.plot(range(len(self.data[6])), self.data[6])
+        self.plot_5.plot(range(len(self.data[8])), self.data[8])
+        self.plot_6.plot(range(len(self.data[10])), self.data[10])
+        self.plot_7.plot(range(len(self.data[12])), self.data[12])
+        self.plot_8.plot(range(len(self.data[14])), self.data[14])
+        
+        self.BCH_1.setText(self.summary[0][0])
+        self.DROOP_1.setText(self.summary[0][1])
+        self.BCH_2.setText(self.summary[1][0])
+        self.DROOP_2.setText(self.summary[1][1])
+        self.BCH_3.setText(self.summary[2][0])
+        self.DROOP_3.setText(self.summary[2][1])
+        self.BCH_4.setText(self.summary[3][0])
+        self.DROOP_4.setText(self.summary[3][1])
+        self.BCH_5.setText(self.summary[4][0])
+        self.DROOP_5.setText(self.summary[4][1])
+        self.BCH_6.setText(self.summary[5][0])
+        self.DROOP_6.setText(self.summary[5][1])
+        self.BCH_7.setText(self.summary[6][0])
+        self.DROOP_7.setText(self.summary[6][1])
+        self.BCH_8.setText(self.summary[7][0])
+        self.DROOP_8.setText(self.summary[7][1])
+        self.BCH_average_1.setText(self.summary[8][0])
+        self.DROOP_average_1.setText(self.summary[8][1])
+        
+
     def measure_2_click(self):
         
         if self.eightbyeight_2.isChecked(): 
